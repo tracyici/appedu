82a48aeb665999a7dfc7fce594bb0950cb77233a
cy <111750403+tracyici@users.noreply.github.com>
2022-08-29 21:36:51 +0800

    course practice

--- 1-7-2-animate/js/index.js
+++ 1-7-2-animate/js/index.js
@@ -1,7 +1,10 @@
+const duration = 500, $rocket = $('#Rocket')
 
 $('#BtnRight').click(function () {
+$rocket.stop(true, false).animate({left:'+=200'}, duration, 'easeOutBack');
 });
 
 $('#BtnLeft').click(function () {
+    $rocket.stop(true, false).animate({left:'-=200'}, duration, 'easeOutBack');
 });
 
