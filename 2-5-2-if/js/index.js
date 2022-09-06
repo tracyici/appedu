d83750829c61251ca353b6db039900a580f6d9af
cy <111750403+tracyici@users.noreply.github.com>
2022-09-06 09:48:00 +0800

    course practice

--- 2-5-2-if/js/index.js
+++ 2-5-2-if/js/index.js
@@ -3,8 +3,10 @@ const $pic = $('#Pic');
 
 $pic.attr('src', './images/ui0' +fileNum+ '.jpg');
 $pic.click(function(){
-   if(fileNum > 4) {
-    './images/ui0' +fileNum-4+ '.jpg'
-   } else {' fileNum = fileNum + 1'};
-//    $(this).attr('src', './images/ui0' +fileNum+ '.jpg');
+//   fileNum = fileNum + 1;
+//   $(this).attr('src', './images/ui0' +fileNum+ '.jpg');
+   if(fileNum >= 4) {
+    fileNum = 1
+   } else {fileNum++};
+   $(this).attr('src', './images/ui0' +fileNum+ '.jpg');
 });
